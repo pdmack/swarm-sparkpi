086db7d68ca587c0746449a329e5753ec874e066
Peter MacKinnon <pmackinn@redhat.com>
2017-07-05 16:43:10 -0400

    WIP using old mainClass (works)

---
+++ src/main/java/io/radanalytics/DefaultEndpoint.java
@@ -0,0 +1,18 @@
+package io.radanalytics;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+
+@Path("/")
+public class DefaultEndpoint {
+    
+    @GET
+    @Produces("text/plain")
+    public Response doGet() {
+        return Response.ok("WildFly Swarm SparkPi server ready").build();
+    }
+
+}
+
---
+++ src/main/java/io/radanalytics/SparkPiSwarmApplication.java
@@ -0,0 +1,21 @@
+package io.radanalytics;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.wildfly.swarm.Swarm;
+import org.wildfly.swarm.jaxrs.JAXRSArchive;
+import org.wildfly.swarm.logging.LoggingFraction;
+
+public class SparkPiSwarmApplication {
+
+    public static void main(String[] args) throws Exception {
+        Swarm swarm = new Swarm();
+
+        JAXRSArchive deployment = ShrinkWrap.create(JAXRSArchive.class, "sparkpi-app.war");
+        deployment.addPackage(SparkPiSwarmApplication.class.getPackage());
+        deployment.addResource(SparkPiEndpoint.class);
+
+        deployment.addAllDependencies();
+        swarm.fraction(LoggingFraction.createDefaultLoggingFraction()).start().deploy(deployment);
+    }
+}
+
